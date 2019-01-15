24dafa6e684ea8b7d047ae260eb3a366f617bd84
Amos Chan <amosschan@gmail.com>
2019-01-14 20:25:41 -0500

    Update footer component

--- src/common/footer.js
+++ src/common/footer.js
@@ -1,28 +1,33 @@
 import "./styles.scss"
 import React from "react"
-import { Container } from "semantic-ui-react"
 import { Link } from "react-router-dom"
 import { loginRoute } from "./url-helper"
+import { Grid, Row, Col } from "react-flexbox-grid"
+import styled from "styled-components"
+import Theme from "./theme"
 
-const Footer = props => {
+const UnstyledFooter = props => {
+  const { className } = props
+
   return (
-    <footer className="footer">
-      <Container textAlign="center">
-        <img src="/cma.png" style={{ width: 40, height: 40, marginBottom: 25 }} />
-        <div style={{ textTransform: "uppercase" }}>
-          Montreal Chinese Alliance Church
-        </div>
-        <a href="https://www.google.ca/maps/place/13+Rue+Finchley,+Hampstead,+QC+H3X+2Z4/@45.4791032,-73.6348992,17z/data=!3m1!4b1!4m2!3m1!1s0x4cc917507e7efe21:0x334fc9316d4951e3">13 Finchley, Hampstead, Québec H3X 2Z4</a> |&nbsp;
-        <a href="tel:5144822703">514.482.2703</a><br />
-        <a href="http://montreal-cac.org/">滿地可華人宣道會</a> |&nbsp;
-        <a href="http://mcac-m.blogspot.ca/">教会网站</a> |&nbsp;
-        <Link to={loginRoute.url()}>Login</Link><br />
-        <div style={{ margin: "25px 0", fontStyle: "italic" }}>
-          Because God loves us, we will love Jesus, love His people, and love the world, for Jesus' sake.
-        </div>
-      </Container>
+    <footer className={`footer ${className}`}>
+      <Grid>
+        <Row>
+          <Col xs={12}>
+            <div>
+              Montreal Chinese Alliance Church is located in Hampstead<br />
+              at <a href="https://www.google.ca/maps/place/13+Rue+Finchley,+Hampstead,+QC+H3X+2Z4/@45.4791032,-73.6348992,17z/data=!3m1!4b1!4m2!3m1!1s0x4cc917507e7efe21:0x334fc9316d4951e3">13 Finchley</a> in Montreal, QC<br />
+              with congregations in <a href="http://montreal-cac.org/">Cantonese</a>, <a href="https://mcac.chuch">English</a> and <a href="http://mcac-m.blogspot.ca/">Mandarin</a>.
+            </div>
+          </Col> 
+        </Row>
+      </Grid>
     </footer>
   )
 }
+
+const Footer = styled(UnstyledFooter)`
+  font-size: ${Theme.fontSize.sm};
+`
 
 export default Footer
---
+++ src/common/theme.js
@@ -0,0 +1,11 @@
+const Theme = {
+  fontSize: {
+    xs: "7px",
+    sm: "12px",
+    md: "14px",
+    lg: "18px",
+    xl: "24px"
+  }
+}
+
+export default Theme
