7c7119e2728829f95e7650d0b3b81dcccf8008a9
Jyo Lewis <jyo.lewis@gmail.com>
2019-07-15 21:45:27 -0400

    Centered series-sermons

--- src/sermons/index.js
+++ src/sermons/index.js
@@ -2,24 +2,46 @@ import Footer from "../common/footer"
 import React from "react"
 import SermonsHeader from "./sermons-header"
 import SeriesList from "./series-list"
-import { Container, Divider, Header } from "semantic-ui-react"
+import { Typography, Divider } from "antd"
+import { Grid, Row, Col } from "react-flexbox-grid"
+import styled from "styled-components"
+import Theme from "../common/theme" 
 
-const Sermons = props => {
-  const { series, onNavBarClick } = props
 
+const { Title } = Typography
+
+const Sermons = ({ className, series, onNavBarClick }) => {
   return (
-    <div>
+    <div className={className}>
       <SermonsHeader onNavBarClick={onNavBarClick} />
-      <Container text style={{ margin: "50px 0" }}>
-        <div style={{ margin: "50px 0" }}>
-          <Header size="tiny">Sermon Series Archive</Header>
+      <Grid>
+          <Row>
+            <Col xsOffset={1} xs={10} mdOffset={2} md={8}>
+            <div className="header">
+          <Title level={2}>Sermon Series Archive</Title>
           <Divider />
         </div>
         <SeriesList series={series} />
-      </Container>
+            </Col>
+          </Row>
+        </Grid>
       <Footer />
     </div>
   )
+ 
 }
 
-export default Sermons
+const StyledSermons = styled (Sermons)` 
+
+.container {
+  margin: ${Theme.spacing.md} auto;
+}
+
+.header {
+  margin-bottom: 50px;
+}
+
+
+` 
+
+export default StyledSermons
